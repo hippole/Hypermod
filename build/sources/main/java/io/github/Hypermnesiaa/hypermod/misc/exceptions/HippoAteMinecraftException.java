905f1978315a7f12184cfc86edaa21aefe8c602f
Hypermnesiaa <kylezeng1522@gmail.com>
2022-02-12 21:20:40 -0500

    remove some stuff because clean

--- build/sources/main/java/io/github/Hypermnesiaa/hypermod/misc/exceptions/HippoAteMinecraftException.java
+++
@@ -1,8 +0,0 @@
-package io.github.Hypermnesiaa.hypermod.misc.exceptions;
-
-public class HippoAteMinecraftException extends Exception {
-
-    public HippoAteMinecraftException() {
-        super("hippo thinks minecraft tastes good");
-    }
-}
